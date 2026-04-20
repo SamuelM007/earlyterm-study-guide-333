a0166c565f43dcc42790a21c49168240c06acb16
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-04-20 10:33:01 -0700

    maxDiff with array

--- src/Practice.java
+++ src/Practice.java
@@ -6,8 +6,16 @@ public class Practice {
      * @return the difference between the largest and smallest number
      */
     public static int maxDiff(int[] nums) {
-        // TODO: implement this
-        return -1;
+        int min = nums[0];
+        int max = nums[0];
+        for(int num : nums){
+            if(num > max){
+                max = num;
+            } else if(num < min){
+                min = num;
+            }
+        }
+        return max-min;
     }
 
 
